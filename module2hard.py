7cfdd770dfef7b26d97f8c043de42e2157862e29
IngaKon <kononova.inga@list.ru>
2024-07-09 13:12:00 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,16 @@
+def ppassword_generator(n):
+    result=""
+    for i in range(1, n):
+        for j in range(i + 1, n):
+            if (i + j) % n == 0:
+                result += str(i) + str(j)
+    return result
+
+
+number = 0
+while number < 3 or number > 20:
+    number = int(input("Введите число для пароля от 3 до 20: "))
+
+password = ppassword_generator(number)
+print(f"Пароль для числа {number}: {password}")
+print("Поздравляю,вы сбежали")
